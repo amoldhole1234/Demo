4a46e02fda39167260710448fbc66a0531537cd1
amoldhole <amoldhole1234@gmail.com>
2021-06-10 11:13:24 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/ChangeWarNameApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeWarNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeWarNameApplication.class, args);
+	}
+
+}
